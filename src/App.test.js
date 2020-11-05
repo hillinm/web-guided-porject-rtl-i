25087b1f734669ead79a604b15ec77b33643106f
Mark Hillin <mark.hillin@gmail.com>
2020-11-04 21:57:31 -0600

    First Test Committed

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,11 @@ import App from "./App";
 test('renders App without errors', () => {
   render(<App />);
 })
+
+test('renders the app header', () => {
+  render(<App />);
+
+  const header = screen.getByText(/add new animal/i);
+
+  expect(header).toBeInTheDocument();
+});
